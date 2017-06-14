eef519369debdc9a0a46b3dcdd69d2c61a74a167
GalinaPo <shine19@bk.ru>
2017-06-14 16:00:11 +0300

    Добавлены классы и объекты

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by gpodmorina on 14.06.2017.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+
+}
