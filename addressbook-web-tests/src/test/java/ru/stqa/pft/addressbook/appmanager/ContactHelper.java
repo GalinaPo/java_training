9400486dc1d3dd7b55b88e5ec4428929b07920b5
GalinaPo <shine19@bk.ru>
2017-07-10 11:09:39 +0300

    красн firstname, lastname

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -2,8 +2,12 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by gpodmorina on 22.06.2017.
@@ -62,5 +66,16 @@ public class ContactHelper extends HelperBase {
 
   public boolean isThereAContact() {
     return isElementPresent(By.name("selected[]"));
+  }
+
+  public List<ContactData> getContactList() {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("td.center"));
+    for (WebElement element : elements) {
+      String name = element.getText();
+      ContactData contact = new ContactData(firstname, lastname, null,null );
+      contacts.add(contact);
+    }
+    return contacts;
   }
 }
